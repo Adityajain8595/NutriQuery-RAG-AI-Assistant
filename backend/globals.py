48a92c5aa2d4ca60ece9c8ed7f5c30c888c8a43a
Adityajain8595 <meaditya1103@gmail.com>
2025-07-26 14:27:28 +0530

    Using endpoint embeddings

--- backend/globals.py
+++ backend/globals.py
@@ -1,4 +1,4 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_community.retrievers import PineconeHybridSearchRetriever
 from pinecone import Pinecone
 from pinecone_text.sparse import BM25Encoder
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
-os.environ['HF_TOKEN'] = os.getenv("HF_TOKEN")
+HF_TOKEN = os.getenv("HF_TOKEN")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
 index_name = "nutriquery-rag-hybrid-search"
@@ -26,7 +26,10 @@ if index_name not in pc.list_indexes():
     )
     
 """
-embeddings = HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
+embeddings = HuggingFaceEndpointEmbeddings(
+    repo_id="https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=HF_TOKEN
+)
 index = pc.Index(index_name)
 BM25_Encoder = BM25Encoder()
 pc_retriever = PineconeHybridSearchRetriever(embeddings=embeddings, sparse_encoder=BM25_Encoder, index=index)
